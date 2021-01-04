844b0d88312368b80f85f61ab6419119be9ee618
GregoryValeryS <GregoryValeryS@gmail.com>
2021-01-04 18:47:23 +0500

    Bidl rename

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # vk_api
-Search and analysis of personal data obtained through the VK API
+Search and analysis of personal data obtained through the VK API.
+The program is called Bigl in honor of the detective character of the work "Dunno on the Moon".
--- main.py
+++ main.py
@@ -1018,8 +1018,8 @@ def main():
         connect_push_button_groups_getMembers_clear_2)
     groups_getMembers[1].pushButton_groups_getMembers_save.clicked.connect(connect_push_button_groups_getMembers_save_2)
 
-    main_menu.textBrowser.append('Программа "Palantir" иницирована и готова к использованию\n'
-                                 'Версия - Alpha 0.4\n'
+    main_menu.textBrowser.append('Программа "Bigl" ("Бигль") готова к использованию\n'
+                                 'Версия - Alpha 0.5\n'
                                  'Связь с автором - GregoryValeryS@gmail.com\n'
                                  'GNU General Public License v3.0\n')
 
